197dd671046c653df4a1d262c6cb32b89cb51473
MarcosFelype <mfs22@aluno.ifal.edu.br>
2025-09-10 12:34:50 -0300

    feat: add test feature

---
+++ src/main/java/com/mballem/demoparkapi/jwt/JwtToken.java
@@ -0,0 +1,15 @@
+package com.mballem.demoparkapi.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtToken {
+    String token;
+
+}
